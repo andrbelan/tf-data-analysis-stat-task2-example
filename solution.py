73ab9457f2d84efc52e7988c4b6ae9b9d6590292
andrbelan <130162109+andrbelan@users.noreply.github.com>
2023-04-07 18:17:30 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,9 +3,10 @@ import numpy as np
 
 from scipy.stats import norm
 
-
 chat_id = 412238846 # Ваш chat ID, не меняйте название переменной
-y = x - 0.083
 
-return max(y)/(pow((1+p)/2,1/n)+0.083, /
-max(y)/(pow((1-p)/2,1/n)+0.083
+def solution(p: float, x: np.array) -> tuple:
+  n = len(x)
+  y = x - 0.083
+  return max(y)/(pow((1+p)/2,1/n)+0.083), max(y)/(pow((1-p)/2,1/n)+0.083)
+
